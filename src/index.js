97634cb14376a9fa3cc6bd8b06667b3710a05641
Drothschild89 <Drothschild89@gmail.com>
2021-01-20 09:27:12 -0800

    got refresh done

--- src/index.js
+++ src/index.js
@@ -1,76 +1,68 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let dogForm = document.getElementById('dog-form')
-    // dogForm.addEventListener('submit', handleSubmit)
-    fetchDogs()
-})
-
-    function fetchDogs(){
+    let form = document.getElementById('dog-form')
+    form.addEventListener('submit', (e) => handleSubmitForm(e))
+    function getAllDogs(){
         fetch('http://localhost:3000/dogs')
         .then(res => res.json())
         .then(dogs => {
-            dogs.forEach(dog => showDogs(dog))
+            dogs.forEach(dog => renderDogs(dog))
         })
     }
+    getAllDogs()
 
-    function showDogs(dog){
-        let tableBody = document.getElementById("table-body")
-        let newRow = document.createElement('tr')
-        newRow.id = dog.id
+    function renderDogs(dog){
+        let table = document.getElementById('table-body')
+
+        let tr = document.createElement('tr')
 
         let name = document.createElement('td')
-        name.textContent = dog.name
+        name.innerText = dog.name
 
         let breed = document.createElement('td')
-        breed.textContent = dog.breed
+        breed.innerText = dog.breed
 
         let sex = document.createElement('td')
-        sex.textContent = dog.sex
-        
-        let editButton = document.createElement('button')
-        editButton.textContent = 'Edit Dog'
-        let editCell = document.createElement('td')
-        editCell.append(editButton)
-        editButton.addEventListener('click', (e) => dogListener(e, dog)) 
+        sex.innerText = dog.sex
 
-        newRow.append(name, breed, sex, editCell)
-        tableBody.appendChild(newRow)
+        let buttonWrapper = document.createElement('td')
+        let button = document.createElement('button')
+        button.innerText = 'Edit Dog'
+        buttonWrapper.appendChild(button)
+        button.addEventListener('click', () => populateForm(dog))
 
+        tr.append(name, breed,sex,buttonWrapper)
+        table.appendChild(tr)
     }
 
-
-    function dogListener(e, dog){
-        let target = e.target.parentElement.parentElement
-        let form = document.querySelector("#dog-form")
-        form.name.value = target.querySelectorAll('td')[0].textContent
-        form.breed.value = target.querySelectorAll('td')[1].textContent
-        form.sex.value = target.querySelectorAll('td')[2].textContent
-        let dogForm = document.getElementById('dog-form')
-        dogForm.addEventListener('submit', (e) => handleSubmit(e, dog))
+    function populateForm(dog){
+        form.name.value = dog.name
+        form.breed.value = dog.breed
+        form.sex.value = dog.sex
+        form.dataset.id = dog.id
     }
 
-    // let dogForm = document.getElementById('dog-form')
-    // dogForm.addEventListener('submit', handleSubmit)
+    function handleSubmitForm(e){
+        e.preventDefault()
+        let table = document.getElementById('table-body')
+        let dogId = e.target.dataset.id
 
-    function handleSubmit(e, dog){
-        e.preventDefault();
-        let id = dog.id
-        fetch(`http://localhost:3000/dogs/${id}`, {
+        fetch(`http://localhost:3000/dogs/${dogId}`, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
             },
             body: JSON.stringify({
-                name: e.target.name.value,
-                breed: e.target.breed.value,
-                sex: e.target.sex.value
-                
+                "name": e.target.name.value,
+                "breed": e.target.breed.value,
+                "sex": e.target.sex.value
             })
         })
         .then(res => res.json())
-        .then(dog => {
-            document.getElementById(dog.id).querySelectorAll('td')[0].textContent = dog.name
-            document.getElementById(dog.id).querySelectorAll('td')[1].textContent = dog.breed
-            document.getElementById(dog.id).querySelectorAll('td')[2].textContent = dog.sex
+        .then(() => {
+            table.innerHTML = ''
+            form.reset()
+            getAllDogs()
         })
     }
+})
